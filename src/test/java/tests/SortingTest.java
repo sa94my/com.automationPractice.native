1cce550bef03a5ac4be375df11ff977ffc1e9882
sa94my <ahmad_samy93@yahoo.com>
2021-06-13 15:32:27 +0200

    separate data

--- src/test/java/tests/SortingTest.java
+++ src/test/java/tests/SortingTest.java
@@ -9,13 +9,15 @@ public class SortingTest extends TestBase{
 
 	NotebooksPage notebooksPageObject ;
 	SoftAssert soft = new SoftAssert();
+	int sortingMethod = 3; // price low to high,when changed change first product id accordingly
+	String dataProductId = "7";
 	@Test
 	public void testSorting() {
 		notebooksPageObject = homeObject.navigateToNotebooksPage();
 		
 		soft.assertEquals(notebooksPageObject.getNameOfFirstProduct(), "4", "default sorting is faulty");
-		notebooksPageObject.sortBy(3);
-		soft.assertEquals(notebooksPageObject.getNameOfFirstProduct(), "7", " sorting is not working properly");
+		notebooksPageObject.sortBy(sortingMethod);
+		soft.assertEquals(notebooksPageObject.getNameOfFirstProduct(), dataProductId, " sorting is not working properly");
 		soft.assertAll();
 	}
 }
